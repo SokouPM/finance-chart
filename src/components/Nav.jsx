efe3f95c7d3c2e77f7b62891fa0fa70fc2036c67
Pierre Marquet <marquet_pierre@yahoo.fr>
2022-01-24 16:28:32 +0100

    add icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link"
+import { MdAddCircle, MdOutlineEventNote } from "react-icons/md"
 
 const Nav = () => {
   return (
     <nav className="text-lg flex flex-row items-center justify-between">
       <Link href="/">
-        <a className="py-2 text-center w-24 transition-all hover:bg-slate-500 hover:font-bold">
-          Journal
+        <a className="py-2 text-center flex items-center justify-evenly w-32 transition-all hover:bg-slate-500 hover:font-bold">
+          <MdOutlineEventNote /> Journal
         </a>
       </Link>
       <Link href="/ajouter">
-        <a className="py-2 text-center w-24 transition-all hover:bg-slate-500 hover:font-bold">
-          Ajouter
+        <a className="py-2 text-center flex items-center justify-evenly w-32 transition-all hover:bg-slate-500 hover:font-bold">
+          <MdAddCircle /> Ajouter
         </a>
       </Link>
     </nav>
